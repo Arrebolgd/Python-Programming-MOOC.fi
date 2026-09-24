89d72c178692785b58cdf90c7ca6cc11b4163d6f
Arrebolgd <arrebolgd@gmail.com>
2026-09-24 15:53:52 +0200

    First commit

---
+++ part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,18 @@
+# Write your solution here
+'''
+Given name: Steve
+Family name: Sanders
+Street address: 91 Station Road
+City and postal code: London EC05 6AW
+Steve Sanders
+91 Station Road
+London EC05 6AW
+'''
+
+name = input("Given name: ")
+lastName = input("Family name: ")
+address = input("Street address: ")
+cityCode = input("City and postal code: ")
+print(name + " " + lastName)
+print(address)
+print(cityCode)
